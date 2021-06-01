e50489050a6be195f5dcb7ef346716a2c30d9dc8
Wilfred Khalik <>
2021-06-01 18:32:52 +0000

    12

--- index.js
+++ index.js
@@ -1,82 +1,55 @@
 // Skeleton
 
 // FUNCTION DEFINITION(S)
-function average(numbers)
+function findMaxRepeatCountInWord(word) 
 {
-  // edge case
-  if(numbers === undefined || numbers === null || !Array.isArray(numbers) || numbers.length === 0)
+  // Break up individual words into individual letters.
+  var characterArray = word.split('');
+  
+  // Object to keep track of each character count
+  var countObject = {};
+  
+  // keep track of the max count
+  var maxCount = 0;
+  
+  // Count the instances of each letter
+  for(var index = 0; index < characterArray.length; index++)
   {
-    return undefined;
+   var currentCharacter = characterArray[index];
+
+  // Iterate all the counts and find the highest 
+   if(countObject[currentCharacter] === undefined)
+   {
+     countObject[currentCharacter] = 1;
+   }
+   else
+   {
+     countObject[currentCharacter]++;
+   }
+   if(countObject[currentCharacter] > maxCount)
+   {
+     maxCount = countObject[currentCharacter];
+   }
   }
-  // uses sum function
-  var average = sum(numbers) / numbers.length;
-  // returns the average of an array of numbers
-  return average;
+
+  // Return this word's max repeat count
+    return maxCount;
 }
 
-function sum(numbers) 
-{
-  // returns the sum of an array of numbers
-  // accumulator
-  var sum = 0;
-  // iterate
-  for(var index = 0; index < numbers.length; index++)
-  {
-    sum += numbers[index];
-  }
-  return sum;
+function findFirstWordWithMostRepeatedChars(text) {
+  var maxRepeatCountOverall = 0;
+  var wordWithMaxRepeatCount = '';
+
+  // Break up input text into words (space-delimited).
+  // For each word...
+    var repeatCountForWord = findMaxRepeatCountInWord(word)
+    // If that max repeat count is higher than the overall max repeat count, then
+      // update maxRepeatCountOverall
+      // update wordWithMaxRepeatCount
+
+  return wordWithMaxRepeatCount;
 }
 
 // ASSERTION FUNCTION(S) TO BE USED
-function assertAverageEquals(actual, expected, testName)
-{
-  if(actual === expected)
-  {
-    console.log('Passed');
-  }
-  else
-  {
-    console.log('FAILED. [' + testName + '] Expected ' + expected + ' , but got ' + actual);  
-  }
-}
 
 // TESTS CASES
-
-// Test Case 1
-var output1 = average([2,4,8,9,12]);
-var expected1 = 7;
-//
-assertAverageEquals(output1, expected1, 'The average of 2,4,8,9 and 12 is 7');
-
-// Test Case 2
-var output2 = average(0);
-var expected2 = undefined;
-//
-assertAverageEquals(output2, expected2, 'The average of 0 is undefined');
-
-// Test Case 3
-var output3 = average([]);
-var expected3 = undefined;
-//
-assertAverageEquals(output3, expected3, 'The average of [] is undefined');
-
-// Test Case 4
-var output4 = average(null);
-var expected4 = undefined;
-//
-assertAverageEquals(output4, expected4, "The average of 'null' is undefined");
-
-// Test Case  5
-var output5 = average([2,-4,8,-9,12]);
-var expected5 = 1.8;
-//
-assertAverageEquals(output5, expected5, 'The average of 2,-4,8,-9 and 12 is 1.8');
-
-// Test Case  6
-var output6 = average([0]);
-var expected6 = 0;
-//
-assertAverageEquals(output5, expected5, 'The average of 0 is 0');
-
-
-
