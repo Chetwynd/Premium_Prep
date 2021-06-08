3614ec703955cb658370d13ebab6db323eaf1f32
Wilfred Khalik <>
2021-06-08 19:07:58 +0000

    Cleaned up code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Skeleton
-
 // FUNCTION DEFINITION(S)
 function findMaxRepeatCountInWord(word) {
   // Break up individual words into individual letters.
@@ -12,14 +10,14 @@ function findMaxRepeatCountInWord(word) {
     
     if(objChar[currentChar] === undefined)
     {
-      obj[currentChar] = 1;
+      objChar[currentChar] = 1;
     }
     else
     {
-      obj[currentChar]++;
+      objChar[currentChar]++;
     }
     // Iterate all the counts and find the highest
-    maxCharCount = Math.max(maxCharCount, obj[currentChar]);
+    maxCharCount = Math.max(maxCharCount, objChar[currentChar]);
   }
   // Return this word's max repeat count
   return maxCharCount;
@@ -64,14 +62,21 @@ function assertEquals(actual, expected, testName)
 
 // TESTS CASES
 
+var testName = 'Check and log to the console the word in a text that has the most number of repeated characters.';
+
 // Test 1
 var test1 = findFirstWordWithMostRepeatedChars('Today is Tuesday, June 8th!');
-assertEquals(test1, 'Today', 'Check and log to the console the word in a text that has the most number of repeated characters.');
+assertEquals(test1, 'Today', testName);
 
 // Test 2
-var test2 = findFirstWordWithMostRepeatedChars(text);
-assertEquals(test2, expected, testName);
+var test2 = findFirstWordWithMostRepeatedChars('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+assertEquals(test2, 'typesetting', testName);
 
 // Test 3
-var test3 = findFirstWordWithMostRepeatedChars(text);
-assertEquals(test3, expected, testName);
+var test3 = findFirstWordWithMostRepeatedChars('There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.');
+assertEquals(test3, 'passages', testName);
+
+
+// Test 4
+var test4 = findFirstWordWithMostRepeatedChars('Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.');
+assertEquals(test4, '2000', testName);
