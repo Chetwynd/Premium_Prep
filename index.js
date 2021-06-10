d05af46d56050ff87209cdae5b8917eb248eab0f
Wilfred Khalik <wilfredkhalik@gmail.com>
2021-06-10 01:00:00 +0000

    Pasted code

--- index.js
+++ index.js
@@ -2,20 +2,25 @@
 
 // FUNCTION DEFINITION(S)
 function findLongestPalindrome(sentence) {
+ 
   // split sentence into words
   var words = sentence.split(' ');
+  
+  var wordsAccumulator = [];
   // iterate words and collect the palindromes
+  for(var index = 0; index < words.length; index++)
+  {
+    var currentWord = words[index];
+
+    if(isPalindrome(currentWord))
+    {
+      wordsAccumulator.push(currentWord);
+    }
+  }
   // sort the list of palindromes by word length
+  wordsAccumulator.sort(sortAscendingByLength);
   // return the largest item in the sorted list
-}
-
-
-function reverseString(string) {
-
-}
-
-function isPalindrome(word) {
-  // hint: you can detect palindromes by comparing a string to its reverse
+  return wordsAccumulator[wordsAccumulator.length - 1];
 }
 
 function sortAscendingByLength(a, b) {
@@ -27,6 +32,56 @@ function sortAscendingByLength(a, b) {
   return 0;
 }
 
+function reverseString(string)
+{
+ var splitStringArray = string.split('');
+ // Do a reverse on the array
+ splitStringArray.reverse();
+
+ // Do a join to get back a string
+ var reverseString = splitStringArray.join('').toLowerCase();
+
+ return reverseString;
+}
+
+function isPalindrome(word) 
+{
+  // hint: you can detect palindromes by comparing a string to its reverse
+  return word === reverseString(word);
+}
+
 // ASSERTION FUNCTION(S) TO BE USED
+function assertEquals(actual, expected, testName)
+{
+  if(actual === expected)
+  {
+    console.log('Passed');
+  }
+  else
+  {
+    console.log('FAILED. Expected ' + expected + ', but got ' + actual);
+  }
+}
 
 // TESTS CASES
+var testName = 'Find the longest Palindrome in the sentence.'
+var sentence1 = 'Madam on no pets';
+var sentence2 = 'Was it a car or a cat I saw';
+var sentence3 = 'Step on no Racecar';
+var sentence4 = 'I did did I';
+
+// // Test 1
+// var actual1 = findLongestPalindrome(sentence1);
+// assertEquals(actual1, 'madam', testName);
+
+// // Test 2
+// var actual2 = findLongestPalindrome(sentence2);
+// assertEquals(actual2, 'a', testName);
+
+// Test 3
+var actual3 = findLongestPalindrome(sentence3);
+assertEquals(actual3, 'racecar', testName);
+
+// // Test 4
+// var actual4 = findLongestPalindrome(sentence4);
+// assertEquals(actual4, 'did', testName);
