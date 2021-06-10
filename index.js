991394fecc5678dce31009c59db29888540a02e6
Wilfred Khalik <wilfredkhalik@gmail.com>
2021-06-10 01:12:45 +0000

    toLowerCase()

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function findLongestPalindrome(sentence) {
  
   // split sentence into words
   var words = sentence.split(' ');
-  
+  //console.log(words);
   var wordsAccumulator = [];
   // iterate words and collect the palindromes
   for(var index = 0; index < words.length; index++)
@@ -37,17 +37,17 @@ function reverseString(string)
  var splitStringArray = string.split('');
  // Do a reverse on the array
  splitStringArray.reverse();
-
+ 
  // Do a join to get back a string
  var reverseString = splitStringArray.join('').toLowerCase();
-
+ 
  return reverseString;
 }
 
 function isPalindrome(word) 
 {
   // hint: you can detect palindromes by comparing a string to its reverse
-  return word === reverseString(word);
+  return word.toLowerCase() === reverseString(word);
 }
 
 // ASSERTION FUNCTION(S) TO BE USED
@@ -70,18 +70,18 @@ var sentence2 = 'Was it a car or a cat I saw';
 var sentence3 = 'Step on no Racecar';
 var sentence4 = 'I did did I';
 
-// // Test 1
-// var actual1 = findLongestPalindrome(sentence1);
-// assertEquals(actual1, 'madam', testName);
+// Test 1
+var actual1 = findLongestPalindrome(sentence1);
+assertEquals(actual1, 'Madam', testName);
 
-// // Test 2
-// var actual2 = findLongestPalindrome(sentence2);
-// assertEquals(actual2, 'a', testName);
+// Test 2
+var actual2 = findLongestPalindrome(sentence2);
+assertEquals(actual2, 'I', testName);
 
 // Test 3
 var actual3 = findLongestPalindrome(sentence3);
-assertEquals(actual3, 'racecar', testName);
+assertEquals(actual3, 'Racecar', testName);
 
-// // Test 4
-// var actual4 = findLongestPalindrome(sentence4);
-// assertEquals(actual4, 'did', testName);
+// Test 4
+var actual4 = findLongestPalindrome(sentence4);
+assertEquals(actual4, 'did', testName);
